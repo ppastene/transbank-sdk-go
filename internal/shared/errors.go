1c44618ec4e36fda3f7fc42671ff3a16cca65d5c
Pedro Pablo Pastene <pe.pastene@gmail.com>
2026-03-04 22:55:28 -0300

    Adding JSON marshaling to the error struct

--- internal/shared/errors.go
+++ internal/shared/errors.go
@@ -1,11 +1,14 @@
 package shared
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type WebpayError struct {
 	Code           int    `json:"code"`
 	ServiceMessage string `json:"service_message"`
-	Cause          error
+	Cause          error  `json:"cause"`
 }
 
 func (e *WebpayError) Error() string {
@@ -17,4 +20,21 @@ func (e *WebpayError) Error() string {
 
 func (e *WebpayError) Unwrap() error {
 	return e.Cause
+}
+
+func (e *WebpayError) MarshalJSON() ([]byte, error) {
+	type Alias WebpayError
+
+	var causeMsg string
+	if e.Cause != nil {
+		causeMsg = e.Cause.Error()
+	}
+
+	return json.Marshal(&struct {
+		*Alias
+		Cause string `json:"cause"`
+	}{
+		Alias: (*Alias)(e),
+		Cause: causeMsg,
+	})
 }
