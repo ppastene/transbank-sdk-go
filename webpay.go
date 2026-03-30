2a546c8f5af6b7be4e71f4590d14b6e664e18700
Pedro Pablo Pastene <pe.pastene@gmail.com>
2026-03-29 22:15:29 -0300

    Include Oneclick types on root #11

--- webpay.go
+++ webpay.go
@@ -2,6 +2,7 @@ package webpay
 
 import (
 	"github.com/ppastene/transbank-sdk-go/internal/shared"
+	"github.com/ppastene/transbank-sdk-go/oneclick"
 	"github.com/ppastene/transbank-sdk-go/webpayplus"
 )
 
@@ -13,20 +14,39 @@ const (
 type WebpayPlusTransaction = webpayplus.Transaction
 type WebpayPlusMallTransaction = webpayplus.MallTransaction
 type WebpayPlusMallDetails = webpayplus.MallDetails
+type OneclickMallInscription = oneclick.MallInscription
+type OneclickMallTransaction = oneclick.MallTransaction
+type OneclickMallDetails = oneclick.MallDetails
 type Options = shared.Options
 
-func NewTransaction(opt *shared.Options) *Transaction {
+func NewTransaction(opt *shared.Options) *WebpayPlusTransaction {
 	return webpayplus.NewTransaction(nil, opt)
 }
 
-func NewTransactionWithClient(client shared.HTTPClientInterface, opt *shared.Options) *Transaction {
+func NewTransactionWithClient(client shared.HTTPClientInterface, opt *shared.Options) *WebpayPlusTransaction {
 	return webpayplus.NewTransaction(client, opt)
 }
 
-func NewMallTransaction(opt *shared.Options) *MallTransaction {
+func NewMallTransaction(opt *shared.Options) *WebpayPlusMallTransaction {
 	return webpayplus.NewMallTransaction(nil, opt)
 }
 
-func NewMallTransactionWithClient(client shared.HTTPClientInterface, opt *shared.Options) *MallTransaction {
+func NewMallTransactionWithClient(client shared.HTTPClientInterface, opt *shared.Options) *WebpayPlusMallTransaction {
 	return webpayplus.NewMallTransaction(client, opt)
+}
+
+func NewOneclickMallInscription(opt *shared.Options) *OneclickMallInscription {
+	return oneclick.NewMallInscription(nil, opt)
+}
+
+func NewOneclickMallInscriptionWithClient(client shared.HTTPClientInterface, opt *shared.Options) *OneclickMallInscription {
+	return oneclick.NewMallInscription(client, opt)
+}
+
+func NewOneclickMallTransaction(opt *shared.Options) *OneclickMallTransaction {
+	return oneclick.NewMallTransaction(nil, opt)
+}
+
+func NewOneclickMallTransactionWithClient(client shared.HTTPClientInterface, opt *shared.Options) *OneclickMallTransaction {
+	return oneclick.NewMallTransaction(client, opt)
 }
